5f944593c0f23dc36c1d04ab102ec8f3c7efda10
Tamerlan Khutuev <t.khutuev@gmail.com>
2022-11-28 13:43:03 +0300

    add liqubase changelogs

--- src/main/java/com/backand/tracker/services/impl/ProjectServiceImpl.java
+++ src/main/java/com/backand/tracker/services/impl/ProjectServiceImpl.java
@@ -6,4 +6,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
+    @Override
+    public Project createNewProject(String name, String descriptions, Long projectOwnerUserId) {
+        return null;
+    }
+
+    @Override
+    public void deleteProject(Long projectId, Long projectOwnerUserId) {
+
+    }
+
+    @Override
+    public void addEmployeeInProject(Long projectId, Long projectOwnerId, Long employeeUserId) {
+
+    }
+
+    @Override
+    public void deleteEmployeeInProject(Long projectId, Long projectOwnerId, Long employeeUserId) {
+
+    }
 }
